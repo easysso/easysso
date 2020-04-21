1de5b9d84a65ad53460da89d6888fa88f0fa2461
ashisa <ashisa78@live.com>
2020-04-21 11:44:26 +0530

    pushing updates

--- App/App/Controllers/HomeController.cs
+++ App/App/Controllers/HomeController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using EasySSOdemo2.Models;
 using Microsoft.AspNetCore.Mvc;
-using EasySSOdemo2.Models;
-using System.IO;
 using Newtonsoft.Json.Linq;
-using System.Text;
+using System;
+using System.Diagnostics;
 using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Threading.Tasks;
 
 
 namespace EasySSOdemo2.Controllers
@@ -48,43 +45,46 @@ namespace EasySSOdemo2.Controllers
         public async Task<IActionResult> success()
         {
 
-    
-    var req = HttpContext.Request;
-    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-    
-    var a = requestBody.Split(new Char[] { '&' });
-    dynamic response = new JObject();
-    var b = a[0].Split("=")[1];
-    
-    Console.WriteLine("Original token : --- {0} ", requestBody);
-    var stream = b;  
-    var handler = new JwtSecurityTokenHandler();
-    var jsonToken = handler.ReadToken(stream);
-    var tokenS = handler.ReadToken(stream) as JwtSecurityToken;
-    
-foreach(var claim in tokenS.Claims)
-{
-    Console.WriteLine(claim.Type + " " + claim.Value);
-    try{
-        response.Add(claim.Type, claim.Value);
-    }
 
-    catch{
+            var req = HttpContext.Request;
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-    }
+            var a = requestBody.Split(new Char[] { '&' });
+            dynamic response = new JObject();
+            var b = a[0].Split("=")[1];
 
-    //this pseudo-code is placeholder for you to implement check for valid tenants who are your customers/users/partners
-    //if(Azure-tenant-is-customer)
-    //{  // proceed with business logic }
-    //
-    //else
-    //{ // Please sign up with solution first ! // fail }
-}
-        return new OkObjectResult(response.ToString());
-    }
-    string Base64Decode(string base64EncodedData) {
- var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
- return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
-}
+            Console.WriteLine("Original token : --- {0} ", requestBody);
+            var stream = b;
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(stream);
+            var tokenS = handler.ReadToken(stream) as JwtSecurityToken;
+
+            foreach (var claim in tokenS.Claims)
+            {
+                Console.WriteLine(claim.Type + " " + claim.Value);
+                try
+                {
+                    response.Add(claim.Type, claim.Value);
+                }
+
+                catch
+                {
+
+                }
+
+                //this pseudo-code is placeholder for you to implement check for valid tenants who are your customers/users/partners
+                //if(Azure-tenant-is-customer)
+                //{  // proceed with business logic }
+                //
+                //else
+                //{ // Please sign up with solution first ! // fail }
+            }
+            return new OkObjectResult(response.ToString());
+        }
+        string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
     }
 }
---
+++ script/easysso.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+if [ $(echo $1 | wc -c) -le 3 ] || [ $(echo $2 | wc -c) -le 3 ]
+then
+	echo App name or location either missing or app name is shorter than permitted. Please specify a unique name that is at least 3 characters.
+	echo syntax: $0 app-name location-name
+else
+	#setting up the variable; no need to edit any other lines after this section
+	appName=$1
+	rgName=$(echo $appName)rg
+	location=$2
+	storage=$(echo $appName |sed s/-//g)storage
+	appUrl=https://$appName.azurewebsites.net
+	curl -Lfs $appUrl
+	if [ "$?" -ne 6 ]
+	then
+		echo web app $appUrl already exist.
+		echo please provide a unique name for the web app.
+		exit
+	fi
+
+	webappName=$(echo $appName)-demo
+	webappUrl=https://$(echo $webappName).azurewebsites.net
+	curl -Lfs $webappUrl
+	if [ "$?" -ne 6 ]
+	then
+		echo web app $webappUrl already exists.
+	        echo please provide a unique name for the web app.
+		exit
+	fi
+
+	replyUrl1=https://jwt.ms
+	replyUrl2=$(echo $appUrl)/.auth/login/aad/callback
+	replyUrl3=$(echo $webappUrl)/success
+
+	#starting execution of steps
+	echo checking connectivity to azure subscription...
+	if [ $(az account show -o tsv --query id 2>&1 |wc -c) -eq 40 ]
+	then
+		echo -e please follow the instructions below to connect your azure subscription...
+		echo	
+		az login -o tsv
+	fi
+
+	#gathering information to set up the environment
+	echo querying tenant id...
+	tenantId=$(az account show -o tsv --query tenantId)
+	echo querying subscription id...
+	subId=$(az account show -o tsv --query id)
+	echo acquiring access token...
+	token=$(az account get-access-token -o tsv --query accessToken)
+	echo creating manifest for resource access...
+	echo '[{
+	"resourceAppId": "00000003-0000-0000-c000-000000000000",
+	"resourceAccess": [
+	{
+	"additionalProperties": null,
+	"id": "e1fe6dd8-ba31-4d61-89e7-88639da4683d",
+	"type": "Scope"
+	}
+	]
+	}]' >manifest.json
+	echo creating and acquiring service principal id...
+	appId=$(az ad app create --display-name $appName --homepage $appUrl --identifier-uris $appUrl --reply-urls $replyUrl1 $replyUrl2 $replyUrl3 --required-resource-accesses @manifest.json -o tsv --query appId)
+
+	#creating resources
+	echo creating resource group $rgName...
+	az group create -n $rgName -l $location
+	echo creating B1 app service plan for demo web app...
+	az appservice plan create -g $rgName -n $(echo $webappName)-plan
+	echo creating demo web app...
+	az webapp create -g $rgName -p $(echo $webappName)-plan -n $webappName
+	echo deploying code to demo web app...
+	az webapp deployment source config-zip -g $rgName -n $webappName --src ../release/App.zip
+	echo creating storage account $storage...
+	az storage account create -n $storage -g $rgName -l $location --sku Standard_LRS --kind StorageV2
+	echo creating function app...
+	az functionapp create --consumption-plan-location $location --name $appName --os-type Windows --resource-group $rgName --runtime dotnet --storage-account $storage --functions-version 2
+
+	echo updating authentication settings...
+	az webapp auth update -g $rgName -n $appName --enabled true --action LoginWithAzureActiveDirectory --aad-allowed-token-audiences $appUrl --aad-client-id $appId --aad-token-issuer-url https://sts.windows.net/$tenantId
+
+	echo updating additional login parameters...
+	#this is needed to get token in JWT format; jsander's blog on access tokens
+	curl -X PUT --header "Authorization: Bearer $token" https://management.azure.com/subscriptions/$subId/resourceGroups/$rgName/providers/Microsoft.Web/sites/$appName/config/authsettings?api-version=2018-02-01 -d "{\"properties\":{\"additionalLoginParams\":[\"resource=$appId\"]}}" -H "Content-Type: application/json"
+
+	echo
+	echo
+	echo all set! Please use the following URLs for to initiate Azure AD login and logout flows...
+	echo =========================================================================================
+	echo login URL:
+	echo ==========
+	echo https://login.microsoftonline.com/$tenantId/oauth2/authorize?response_type=id_token\&redirect_uri=$replyUrl3\&client_id=$appId\&scope=openid+profile+email\&response_mode=form_post\&resource=\&nonce=fb57942e3d0f43698c83e4b923b68470_20200406130225
+
+	echo logout URL:
+	echo ===========
+	echo https://login.microsoftonline.com/common/oauth2/v2.0/logout
+fi
